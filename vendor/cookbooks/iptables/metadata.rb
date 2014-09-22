cb245fba3727ae146a369789c4a449f87c62484d
vagrant <vagrant@precise64.(none)>
2014-09-22 13:32:44 +0000

    added fail2ban added cookbook for overriding sshd

---
+++ vendor/cookbooks/iptables/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "multunus"
+maintainer_email ""
+license          "Apache 2.0"
+description      "Configures iptables"
+long_description ""
+version          "0.1.0"
+depends "sudo", "> 1.2.0"
+depends "diptables", '~> 0.1.6'
